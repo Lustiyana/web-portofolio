8c4cece158266ca75dba1304128924e08e1fa82f
Lustiyana <Lustiyana1801@gmail.com>
2023-09-18 03:10:09 +0700

    add portofolio

--- script/navbar.js
+++ script/navbar.js
@@ -8,3 +8,14 @@ function Menu(e) {
       list.classList.remove("top-[72px]"),
       list.classList.remonve("opacity-100"));
 }
+
+function downloadPDF() {
+  const pdfUrl = "../assets/documents/cv.pdf"; // Replace with the actual path to your PDF file
+  const a = document.createElement("a");
+  a.href = pdfUrl;
+  a.download = "../assets/documents/cv.pdf"; // Specify the desired file name
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
